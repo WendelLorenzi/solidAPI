218312297a9a1bb3355c7743f864f2eb956d1331
WendelLorenzi <wlorenzi17@gmail.com>
2023-03-30 19:35:54 -0300

    feat: implement User UseCase and Controller

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,33 @@
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvidet";
+import { IUsersRepository } from "../../repositories/IUserRepository";
+import { CreateUserRequestDTO } from "./CreateUserDTO";
+
 export class CreateUserUseCase {
-    async execute() {
-        
+    constructor(
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
+    ) {}
+    async execute(
+        data: CreateUserRequestDTO
+    ) {
+        const userAlreadExists = await this.usersRepository.findByEmail(data.email);
+        if (userAlreadExists) {
+            throw new Error('User alread exists');
+        }
+        const user = new User(data);
+        await this.usersRepository.save(user);
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from: {
+                name: 'Squad BAT',
+                email: 'TI@batcavernacp.com',
+            },
+            subject: 'Seja bem-vindo a plataforma',
+            body: '<p> Voce ja pode fazer login em nossa plataforma.</p>'
+        });
     }
 }
